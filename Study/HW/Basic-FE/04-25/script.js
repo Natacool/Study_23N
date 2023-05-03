c05f8f140903c58c2f10083610ee339b7d91eae2
Natacool <natacool812@gmail.com>
2023-05-03 22:17:56 +0200

    HW from 04-25

---
+++ Study/HW/Basic-FE/04-25/script.js
@@ -0,0 +1,41 @@
+
+// for(let i = 0; i <= 10; i+=2) {
+//     console.log(i)
+// }
+
+
+// for(let i = 55; i >= 20; i--) {
+//     console.log(i)
+// }
+
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6] 
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i])    
+// }
+
+// const numbers_squared = []
+// for (let i = 0; i < numbers.length; i++) {
+//     numbers_squared[i] = numbers[i] **2
+//     console.log("numbers  SQuared ",numbers_squared[i])    
+// }
+
+// const last_elem = numbers_squared[numbers_squared.length-1]
+// console.log("last_elem", last_elem)
+
+let age = 14
+
+const user = {
+    first_name: 'Ivan', 
+    last_name: 'Ivanov', 
+    age: 20, 
+    salary: 500
+    }
+
+let userInfo = `Users name is ${user.first_name} ${user.last_name}.` +
+  " He is " +
+ user.age +
+ " years old"
+console.log (userInfo)
+    
+ 
