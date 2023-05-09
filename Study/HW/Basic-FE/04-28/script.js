c14d9548fd9721e458e2a6efa50a0e912422b8cd
Natacool <natacool812@gmail.com>
2023-05-09 08:35:17 +0200

    HW 05-04

---
+++ Study/HW/Basic-FE/04-28/script.js
@@ -0,0 +1,9 @@
+let firstNumber = +prompt("input the first number")
+let secondNumber = +prompt("input the second number")
+for (let i = 0; i = length.number; i += 2)  {
+    if (firstNumber < secondNumber) {}
+
+}
+console.log("the most", secondNumber)
+
+
