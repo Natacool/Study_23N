5834b06ce236792438951528c2247ea2d8131cd0
Natacool <natacool812@gmail.com>
2023-05-18 16:38:57 +0200

    Create BJhw2.java
    java hw 17.05

---
+++ Study/HW/Java/05-17/BJhw2.java
@@ -0,0 +1,16 @@
+package BasicJavaHw;
+
+
+public class BJhw2 {
+    public static void main(String[] args) {
+        char q = '4';
+        String preResult = String.valueOf(q);
+        int result = Integer.parseInt(preResult);
+        //int result = Integer.parseInt(String.valueOf(q));
+        System.out.println(result+6);
+
+        int result2 = Character.getNumericValue(q);
+        System.out.println(result2 + 6);
+
+    }
+}
