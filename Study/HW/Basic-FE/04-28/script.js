45b2a5b6e9efecb890dbbb47ba35f38459deb70d
Natacool <natacool812@gmail.com>
2023-06-03 21:07:14 +0200

    Update script.js
    Update HW 28.04

--- Study/HW/Basic-FE/04-28/script.js
+++ Study/HW/Basic-FE/04-28/script.js
@@ -1,9 +1,82 @@
-let firstNumber = +prompt("input the first number")
-let secondNumber = +prompt("input the second number")
-for (let i = 0; i = length.number; i += 2)  {
-    if (firstNumber < secondNumber) {}
 
+// ////////////////////////// printEvenNumbers
+// let numberOne = +prompt("input number one")
+// let numberTwo = +prompt ("input number two")
+
+// function printEvenNumbers(firstNumber, secondNumber) {
+//     let max, min
+//     if (firstNumber >= secondNumber) {
+//         max = firstNumber 
+//         min = secondNumber
+//     }
+//     else {
+//         min = firstNumber 
+//         max = secondNumber
+//     } 
+//     for (let i = max; i >= min; i--)  {
+//         if (i % 2 == 0) {
+//             console.log("even number: ", i)
+//         }
+
+//     }
+// }
+
+// printEvenNumbers(numberOne, numberTwo)
+
+// ////////////////////////// power
+
+// let base = +prompt("input a number")
+// let pow = +prompt("input a power")
+
+// function power(b, p) {
+
+//     let p2 = 2
+//     if (p >= 2) {
+//         p2 = p
+//     }
+
+//     return b**p2
+//     }
+    
+//     console.log("base:", base, 
+//                 "in power: ", pow, ", result =", power(base, pow))
+
+///////////////////////////////////////
+
+// function calc_sum(n) {
+//     //console.log("arg = ",n)
+//     let sum = 0
+
+//     for (let i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+
+//   return sum
+// }
+
+
+// let inN = +prompt("Input a number")
+
+// console.log("sum = ", calc_sum(inN))
+
+
+
+let numberOne = +prompt("input number one")
+let numberTwo = +prompt ("input number two")
+function calc_sum(n, m) {
+ 
+  let sum = 
+
+  for (let i = n; i <= m; i++) {
+     
+      if (i % 2 == 0) 
+      
+  }
+
+return sum
 }
-console.log("the most", secondNumber)
 
 
+
+
+console.log("sum = ", calc_sum(n + m))
