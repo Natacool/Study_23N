3452db7654ce905bc358e278aafb871d219e96cf
Natacool <natacool812@gmail.com>
2023-06-23 19:55:53 +0200

    renamed to . SQL

--- Study/HW/GeneralTech/06-15/hw-15-06.sql
+++ Study/HW/GeneralTech/06-15/hw-15-06.sql
@@ -1,7 +1,7 @@
 
  
 
-Вывести название и стоимость в USD одного самого дорогого проданного товара
+--1 Вывести название и стоимость в USD одного самого дорогого проданного товара
 
 SELECT 
 	ProductName,
@@ -10,9 +10,9 @@ FROM Products
 JOIN OrderDetails ON Products.ProductID=OrderDetails.ProductID
 JOIN Orders ON Orders.OrderID=OrderDetails.OrderID
     ORDER BY Price DESC
-	Limit 1
+	Limit 1;
 
-Вывести два самых дорогих товара из категории Beverages из USA
+--2 Вывести два самых дорогих товара из категории Beverages из USA
 
 SELECT *
 FROM Products
@@ -23,9 +23,9 @@ WHERE
     and
     Suppliers.Country='USA'
 	ORDER BY Products.Price DESC
-	LIMIT 2
+	LIMIT 2;
 
-Вывести заказы, добавив поле доставлено (или нет), учитывая, что доставлены только 10248 и 10249
+--3 Вывести заказы, добавив поле доставлено (или нет), учитывая, что доставлены только 10248 и 10249
 
 SELECT *, 
 	CASE
@@ -33,24 +33,24 @@ SELECT *,
     WHEN OrderID = 10249  THEN 'Delivered'
     ELSE 'Not_Delivered'
     END AS Product_Status
-FROM Orders
+FROM Orders;
 
-Вывести список стран, которые поставляют морепродукты
+--4 Вывести список стран, которые поставляют морепродукты
 
 SELECT DISTINCT Country
 FROM Suppliers
 JOIN Products ON Suppliers.SupplierID=Products.SupplierID
 JOIN Categories ON Products.CategoryID=Categories.CategoryID
 WHERE 
-	Categories.CategoryName='Seafood'
+	Categories.CategoryName='Seafood';
 
 
-Очистить поле ContactName у всех клиентов не из China
+--5 Очистить поле ContactName у всех клиентов не из China
 
 UPDATE Customers
 SET ContactName=''
 WHERE
-	NOT Country IN ('China')
+	NOT Country IN ('China');
 
 
 
